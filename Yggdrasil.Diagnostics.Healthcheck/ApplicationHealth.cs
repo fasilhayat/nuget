a3f4a0d1615a614590d5f0a62f22ce42467aaddc
Fasil Malik Hayat <fasil.hayat@gmail.com>
2025-05-25 17:19:18 +0200

    Initial commit

---
+++ Yggdrasil.Diagnostics.Healthcheck/ApplicationHealth.cs
@@ -0,0 +1,100 @@
+﻿namespace Yggdrasil.Diagnostics.Healthcheck;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+/// <summary>
+/// Enables common Integration capabilities for Health
+/// </summary>
+public static class ApplicationHealth
+{
+    /// <summary>
+    /// Adds health checks to the application and exposes a health endpoint.
+    /// </summary>
+    /// <param name="builder">
+    /// The <see cref="WebApplicationBuilder"/> used to configure services and middleware.
+    /// </param>
+    /// <returns>
+    /// The same <see cref="WebApplicationBuilder"/> instance for chaining.
+    /// </returns>
+    /// <example>
+    /// Example usage:
+    /// <code>
+    /// var builder = WebApplication.CreateBuilder(args);
+    /// builder.AddHealth(); // Adds health checks and configures the endpoint
+    ///
+    /// var app = builder.Build();
+    /// app.UseHealth();     // Maps the health check endpoint
+    /// app.Run();
+    /// </code>
+    /// </example>
+    public static WebApplicationBuilder AddHealth(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddHealth();
+        return builder;
+    }
+
+    /// <summary>
+    /// Add standard Health services part of a WebApplication.
+    /// This will standardize how the service report Health.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/> used in add</param>
+    /// <returns><see cref="IServiceCollection"/> with added HealthCheck</returns>
+    public static IServiceCollection AddHealth(this IServiceCollection services)
+    {
+        services.AddHealthChecks();
+        return services;
+    }
+
+    /// <summary>
+    /// Use standard Health.
+    /// </summary>
+    /// <param name="app"><see cref="IEndpointRouteBuilder"/> used for build</param>
+    /// <returns><see cref="IEndpointRouteBuilder"/> used for build</returns>
+    /// <remarks>Used in combination with <see cref="AddHealth(WebApplicationBuilder)"/> or <see cref="AddHealth(IServiceCollection)"/></remarks>
+    public static IEndpointRouteBuilder UseHealth(this IEndpointRouteBuilder app)
+    {
+        app.MapHealthChecks("/healthz", new()
+        {
+            Predicate = _ => true,
+            ResponseWriter = HealthWriter.WriteHealthUiResponse
+        }).WithMetadata(new AllowAnonymousAttribute());
+
+        return app;
+    }
+
+    /// <summary>
+    /// Create a HealthBuilder from a service collection.
+    /// This will standardize how to build service Health checks.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/> used in create</param>
+    /// <returns><see cref="IHealthChecksBuilder"/> with added standard Check</returns>
+    public static IHealthChecksBuilder CreateHealthChecksBuilder(this IServiceCollection services)
+    {
+        var healthChecksBuilder = services.AddHealthChecks().AddCheck("self", () => HealthCheckResult.Healthy());
+        return healthChecksBuilder;
+    }
+
+    /// <summary>
+    /// Add standard Url HealthChecks as part of a builder pattern.
+    /// </summary>
+    /// <param name="builder"><see cref="IHealthChecksBuilder"/> used in add</param>
+    /// <param name="url"> url to external endpoint</param>
+    /// <param name="tag"> name to filter endpoints</param>
+    /// <returns><see cref="IHealthChecksBuilder"/> with added HealthChecks</returns>
+    public static IHealthChecksBuilder AddUrlHealthCheck(this IHealthChecksBuilder builder, Uri url, string tag)
+    {
+        builder.AddUrlGroup( // This method is part of the HealthChecks.Network package
+            uri: url,
+            name: tag,
+            tags: new[] { tag },
+            failureStatus: HealthStatus.Unhealthy,
+            timeout: new TimeSpan(0, 0, 0, 3)
+        );
+
+        return builder;
+    }
+}
---
+++ Yggdrasil.Diagnostics.Healthcheck/HealthWriter.cs
@@ -0,0 +1,151 @@
+﻿namespace Yggdrasil.Diagnostics.Healthcheck;
+
+using HealthChecks.UI.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Specialized version of UIResponseWriter part of
+/// https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks/tree/master/src/HealthChecks.UI.Client
+/// </summary>
+public static class HealthWriter
+{
+    private const string DefaultContentType = "application/json";
+    private static readonly byte[] EmptyResponse = "{}"u8.ToArray();
+    private static readonly Lazy<JsonSerializerOptions> Options = new(CreateJsonOptions);
+    private static List<string>? _assemblies;
+
+    /// <summary>
+    /// Write the Health in HealthUI format
+    /// </summary>
+    /// <param name="httpContext"></param>
+    /// <param name="report"></param>
+    /// <returns></returns>
+    public static async Task WriteHealthUiResponse(HttpContext httpContext, HealthReport? report)
+    {
+        if (report != null)
+        {
+            httpContext.Response.ContentType = DefaultContentType;
+            var uiReport = GenerateReport(report);
+            await JsonSerializer.SerializeAsync(httpContext.Response.Body, uiReport, Options.Value);
+        }
+        else
+        {
+            await httpContext.Response.WriteAsync(Encoding.UTF8.GetString(EmptyResponse));
+        }
+    }
+
+    /// <summary>
+    /// Write the Health in HealthUI format
+    /// </summary>
+    /// <param name="report">The health report to write.</param>
+    /// <returns>Returns a stream containing the serialized health report in HealthUI format.</returns>
+    public static Stream WriteHealthUiResponse(HealthReport? report)
+    {
+        MemoryStream memoryStream;
+        if (report != null)
+        {
+            var uiReport = GenerateReport(report);
+            memoryStream = new();
+            JsonSerializer.Serialize(memoryStream, uiReport, typeof(UIHealthReport), Options.Value);
+            memoryStream.Position = 0;
+
+            return memoryStream;
+        }
+
+        memoryStream = new(EmptyResponse);
+        return memoryStream;
+    }
+
+    /// <summary>
+    /// Generates a UIHealthReport from the provided HealthReport.
+    /// </summary>
+    /// <param name="report">Report to convert.</param>
+    /// <returns>The generated UIHealthReport.</returns>
+    private static UIHealthReport GenerateReport(HealthReport? report)
+    {
+        var entries = new Dictionary<string, HealthReportEntry>();
+        if (report?.Entries != null)
+        {
+            // Transfer existing entries if exists
+            entries = report.Entries.Select(dict => dict).ToDictionary(pair => pair.Key, pair => pair.Value);
+        }
+
+        entries.Add("Assembly info", new HealthReportEntry(report.Status, Assembly.GetEntryAssembly()?.FullName ?? "NA", TimeSpan.Zero, null, null, new string[] { "version" }));
+
+        // Only get assemblies once as it is a heavy task
+        if (_assemblies == null)
+        {
+            _assemblies = new();
+            AppDomain.CurrentDomain.GetAssemblies().ToList().ForEach(assembly =>
+            {
+                if (assembly.FullName != null) _assemblies.Add(assembly.FullName);
+            });
+        }
+
+        entries.Add("Assemblies", new HealthReportEntry(report.Status, string.Join("<br>", _assemblies), TimeSpan.Zero, null, null, new string[] { "version" }));
+        var healthReport = new HealthReport(entries, report.Status, report.TotalDuration);
+
+        var uiReport = UIHealthReport.CreateFrom(healthReport);
+        return uiReport;
+    }
+
+    /// <summary>
+    /// Generates the JSON serializer options used for serialization of the health report.
+    /// </summary>
+    /// <returns>The configured <see cref="JsonSerializerOptions"/>.</returns>
+    private static JsonSerializerOptions CreateJsonOptions()
+    {
+        var options = new JsonSerializerOptions
+        {
+            AllowTrailingCommas = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+
+        options.Converters.Add(new JsonStringEnumConverter());
+
+        // for compatibility with older UI versions ( <3.0 ) we arrange
+        // timespan serialization as s
+        options.Converters.Add(new TimeSpanConverter());
+
+        return options;
+    }
+}
+
+/// <summary>
+/// For compatibility with older UI versions ( less than 3.0 ) we arrange timespan serialization as string.
+/// </summary>
+internal class TimeSpanConverter : JsonConverter<TimeSpan>
+{
+    /// <summary>
+    /// Reads a TimeSpan value from the JSON reader.
+    /// </summary>
+    /// <param name="reader">Ref to the JSON reader.</param>
+    /// <param name="typeToConvert">the type to convert.</param>
+    /// <param name="options"></param>
+    /// <returns>The TimeSpan value read from the JSON.</returns>
+    public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return default;
+    }
+
+    /// <summary>
+    /// Writes a TimeSpan value to the JSON writer as a string representation of the TimeSpan.
+    /// </summary>
+    /// <param name="writer">The JSON writer to write to.</param>
+    /// <param name="value">The value to write.</param>
+    /// <param name="options">Options for the JSON serialization.</param>
+    public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
